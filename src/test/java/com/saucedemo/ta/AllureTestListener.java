42cf788497626c333a8896976dd25d6e338d74c5
kliver <kliver1998@gmail.com>
2025-04-02 21:43:47 -0500

    Add configuration for logger, allure report, ignore some folders

---
+++ src/test/java/com/saucedemo/ta/AllureTestListener.java
@@ -0,0 +1,9 @@
+package com.saucedemo.ta;
+
+import io.qameta.allure.junit5.AllureJunit5;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(AllureJunit5.class)
+public class AllureTestListener {
+}
+
--- src/test/java/com/saucedemo/ta/page/BasePage.java
+++ src/test/java/com/saucedemo/ta/page/BasePage.java
@@ -4,10 +4,10 @@ import org.openqa.selenium.WebDriver;
 
 public abstract class BasePage {
 
-    protected WebDriver webDriver;
+    protected WebDriver driver;
     protected final int WAIT_TIMEOUT_MS = 10 * 1000;
 
-    protected BasePage(WebDriver webDriver) {
-        this.webDriver = webDriver;
+    protected BasePage(WebDriver diver) {
+        this.driver = diver;
     }
 }
